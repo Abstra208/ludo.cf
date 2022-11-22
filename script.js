61c12c7d5daa737aedfdcf7c74e9cd2dba35df6e
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2022-11-21 21:06:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+home.onclick = () => document.getElementById("page").class="home";
+moi.onclick = () => document.getElementById("page").class="moi";
+
+
+
+
+
+/*
+function Home(){
+    document.getElementById("page").class="home";
+}
+function Projet(){
+    document.getElementById("page").class="projet";
+}
+function Moi(){
+    document.getElementById("page").class="moi";
+}
+*/
