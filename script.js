dd6d2962629fc5113227edae4927fe7e3e594dbd
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2022-11-22 12:38:15 -0500

    fix?

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 
-home.onclick = () => document.getElementById("page").class="home";
-moi.onclick = () => document.getElementById("page").class="moi";
-projet.onclick = () => document.getElementById("page").class="projet";
+document.getElementById("home").onclick = () => document.getElementById("page").class="home";
+document.getElementById("moi").onclick = () => document.getElementById("page").class="moi";
+document.getElementById("projet").onclick = () => document.getElementById("page").class="projet";
