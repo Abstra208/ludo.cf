86ab86dbcc04542288329bb77fdf6fc59950b187
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2022-11-23 13:13:30 -0500

    add logo

--- script.js
+++ script.js
@@ -3,3 +3,5 @@
 document.getElementById("home").onclick = () => document.getElementById("page").className="home";
 document.getElementById("moi").onclick = () => document.getElementById("page").className="moi";
 document.getElementById("projet").onclick = () => document.getElementById("page").className="projet";
+
+document.getElementById("logo").onclick = () => document.getElementById("page").className="home";
