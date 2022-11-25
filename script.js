dcd9e24dca99aa60d83add18eecc33f1ffe786d3
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2022-11-24 20:20:16 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("home").onclick = () => document.getElementById("page").className="home";
-document.getElementById("moi").onclick = () => document.getElementById("page").className="moi";
-document.getElementById("projet").onclick = () => document.getElementById("page").className="projet";
-document.getElementById("logo").onclick = () => document.getElementById("page").className="home";
